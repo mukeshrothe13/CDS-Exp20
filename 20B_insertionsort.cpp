dd4ab45e8378165f4d20e2113242abbb0a8c3a6d
mukeshrothe13 <rothemukesh7@gmail.com>
2024-10-21 19:56:38 +0530

    20B_insertionsort.cpp

--- 20B_insertionsort.cpp
+++ 20B_insertionsort.cpp
@@ -22,7 +22,7 @@ int main(){
         cout<<arr[i]<<" ";
     }
     insertionsort(arr,n);
-    cout<< "\n Sorted array: ";
+    cout<< "\nSorted array: ";
     for (int i = 0; i< n ; i++){
         cout << arr[i]<<" ";
     }
